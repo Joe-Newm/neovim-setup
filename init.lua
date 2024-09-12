dfbbdb4e50f7a224298b76b1a70e7754d2d47212
Joe-Newm <j.newm500@gmail.com>
2024-09-12 11:51:18 -0500

    updated lsps and add ts_ls

--- init.lua
+++ init.lua
@@ -662,7 +662,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        ts_ls = {},
         --
 
         -- Java (jdtls)
